5e0e7372895a6329d3a81c3af75aba0641c08bca
Amani <weslatiamani86@gmail.com>
2024-09-29 22:55:51 +0200

    configuration setup , config-server and discovery

---
+++ services/discovery/src/test/java/com/amani/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amani.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
